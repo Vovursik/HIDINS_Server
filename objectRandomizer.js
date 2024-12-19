dd0b02ab75faa9f9a465aae35a54314bcdef95a7
Vladimir <vovavaisik.lyz@gmail.com>
2024-12-19 23:07:57 +0300

    Delete objectRandomizer.js

--- objectRandomizer.js
+++
@@ -1,8 +0,0 @@
-function generateRandomNumbers(n) {
-    const numbers = [];
-    for (let i = 0; i < n; i++) {
-        const randomNumber = Math.floor(Math.random() * 20);  // Генерируем случайное число от 0 до 19
-        numbers.push(randomNumber);
-    }
-    return numbers;
-}
